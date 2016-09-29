a189aaea31da48952ee139efa135526341ac4da0
Alexis <alexisvt@gmail.com>
2016-09-29 15:11:19 -0600

    adding the sagas

--- saga-pratices/src/index.jsx
+++ saga-pratices/src/index.jsx
@@ -4,12 +4,21 @@ import { createStore, applyMiddleware } from 'redux';
 
 import createSagaMiddleware from 'redux-saga';
 import reducer from './reducers';
+import Counter from './Counter';
+import logger from 'redux-logger';
 
-import { helloSaga } from './sagas';
+import rootSaga from './sagas';
 
 const sagaMiddleware = createSagaMiddleware();
-const middlewares = applyMiddleware(sagaMiddleware);
+const middlewares = applyMiddleware(sagaMiddleware, logger());
 
 const store = createStore(reducer, middlewares);
 
-sagaMiddleware.run(helloSaga);
+const action = type => store.dispatch({ type });
+
+sagaMiddleware.run(rootSaga);
+
+function render() { ReactDOM.render(<Counter value={store.getState()} onIncrementAsync={() => action('INCREMENT_ASYNC')} ></Counter>, document.getElementById('main')); }
+
+render();
+store.subscribe(render);
