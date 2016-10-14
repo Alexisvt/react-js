50da10d58c72ab1c4aac057797e8bab703ad4ef5
Alexis <alexisvt@gmail.com>
2016-10-14 15:54:52 -0600

    adding test support to the project

---
+++ pluralsight-sample/src/components/course/CourseForm.test.js
@@ -0,0 +1,9 @@
+// @flow
+import expect from 'expect';
+import * as React from 'react';
+import TestUtils from 'react-addons-test-utils';
+import CourseForm from './CourseForm';
+
+describe('CourseForm via React Test Utils', () => {
+  it('renders from and h1', () => { });
+});
